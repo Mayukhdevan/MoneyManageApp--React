49ff3fa6f517af856a840c22c973ef8a436514fe
Mayukh Devan Sahu <94803484+Mayukhdevan@users.noreply.github.com>
2023-01-29 19:30:33 +0530

    li to div
    changed list item to div

--- src/components/MoneyDetails/index.js
+++ src/components/MoneyDetails/index.js
@@ -4,7 +4,7 @@ const MoneyDetails = props => {
   const {amount, imgData, typeText, dataTestId} = props
 
   return (
-    <li className={`money-details-item ${imgData.altText}`}>
+    <div className={`money-details-item ${imgData.altText}`}>
       <img src={imgData.imgUrl} alt={imgData.altText} />
       <div className="amount-text-container">
         <p className="type-text">Your {typeText}</p>
@@ -12,7 +12,7 @@ const MoneyDetails = props => {
           Rs {amount}
         </p>
       </div>
-    </li>
+    </div>
   )
 }
 
